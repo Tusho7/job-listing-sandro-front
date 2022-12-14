6da452d74e3b72b19b36de73ba905fe1350892b2
Sandro Tushurashvili <sandrotushurashvili7@gmail.com>
2022-12-14 21:47:54 +0400

    feat:get data from api with axios

--- src/components/Jobs.tsx
+++ src/components/Jobs.tsx
@@ -1,9 +1,42 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import Job from "./Job";
 
-function Jobs() {
-  return (
-    <div>Jobs</div>
-  )
+export interface Type {
+  map: any;
+  id: number;
+  company: string;
+  logo: string;
+  new: boolean;
+  featured: boolean;
+  position: string;
+  role: string;
+  level: string;
+  postedAt: string;
+  contract: string;
+  location: string;
+  languages: string[];
+  tools: string[];
 }
 
-export default Jobs
+function Jobs() {
+  const [data, setData] = useState<Type[]>([]);
+  useEffect(() => {
+    const getData = async () => {
+      const res = await axios.get("http://localhost:3000/api/listings");
+      setData(res.data);
+    };
+
+    getData();
+  }, []);
+
+  return (
+    <div>
+      {data.map((object) => {
+        return <Job data={object} />;
+      })}
+    </div>
+  );
+}
+
+export default Jobs;
