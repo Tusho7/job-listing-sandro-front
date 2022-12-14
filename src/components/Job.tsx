b35cd62bbacd5041111fc21ea5fa74c47142e1ec
Sandro Tushurashvili <sandrotushurashvili7@gmail.com>
2022-12-14 21:50:11 +0400

    feat: add all data contents in divs, use map for buttons

--- src/components/Job.tsx
+++ src/components/Job.tsx
@@ -1,9 +1,45 @@
-import React from 'react'
+import React from "react";
+import { Type } from "./Jobs";
 
-function Job() {
+function Job({ data }: { data: Type }) {
+  let items: string[] = [
+    data.role,
+    data.level,
+    ...data.languages,
+    ...data.tools,
+  ];
   return (
-    <div>Job</div>
-  )
+    <div>
+      {data && (
+        <div className="job-container">
+          <div className="logo">
+            <img src={data.logo} alt="" />
+          </div>
+          <div className="company-name">
+            <p>{data.company}</p>
+
+            <div>
+              {data.new && <span className="new">NEW!</span>}
+              {data.featured && <span className="featured">FEATURED</span>}
+            </div>
+          </div>
+          <div className="position">{data.position}</div>
+          <div className="days-time">
+            <span>{data.postedAt}</span>
+            <span>{data.contract}</span>
+            <span>{data.location}</span>
+          </div>
+          <hr></hr>
+
+          <div>
+            {items.map((i) => {
+              return <button>{i}</button>;
+            })}
+          </div>
+        </div>
+      )}
+    </div>
+  );
 }
 
-export default Job
+export default Job;
